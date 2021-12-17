00d707c0a417fafdf91bcc34c437a23d6f0c43da
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-17 11:26:35 -0500

    segundo selector anidado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { PaisesModule } from './paises/paises.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PaisesModule
+    PaisesModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
